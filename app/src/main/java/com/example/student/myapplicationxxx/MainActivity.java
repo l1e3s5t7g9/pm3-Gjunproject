2dc49d619f1cbc67dfbdc9c85f77c3f123c27ab8
studentE1 <studentE1@student>
2017-10-11 17:48:26 +0800

    20171011-新增提示wifi開啟功能

--- app/src/main/java/com/example/student/myapplicationxxx/MainActivity.java
+++ app/src/main/java/com/example/student/myapplicationxxx/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.student.myapplicationxxx;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -29,7 +32,9 @@ public class MainActivity extends AppCompatActivity {
 //                runLoging();                               //執行登入程序
 
         // ==========檢查Network==========
-        Log.d("LOG", String.valueOf(Net.NetCheck(this)));
+        if (Net.NetCheck(this) == false) {
+            openWifi();                            // 提示開啟WIFI
+        }
 
         settinglink = (ImageButton) findViewById(R.id.settinglink);
         messagelink = (ImageButton) findViewById(R.id.messagelink);
@@ -40,6 +45,32 @@ public class MainActivity extends AppCompatActivity {
         Intent in = new Intent();
         in.setClass(MainActivity.this, Start.class);
         startActivity(in);
+
+    }
+
+    public void openWifi() {
+
+        //若wifi狀態為關閉則將它開啟
+        WifiManager wfm = ((WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE));
+        if (!wfm.isWifiEnabled()) {
+
+            new AlertDialog.Builder(this)
+                    .setMessage("應用程式要求開啟 Wi-Fi 功能。")
+                    .setPositiveButton("允許", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ((WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE)).setWifiEnabled(true);
+                        }
+                    })
+                    .setNegativeButton("拒絕", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ;
+                        }
+                    })
+                    .show();
+
+        }
 
     }
 }
