218e48788ebf92c3a18eb7efff934375a0a43878
studentE1 <studentE1@student>
2017-10-12 10:58:59 +0800

    20171012-建立cover class 讓開場可以順暢轉畫面。

--- app/src/main/java/com/example/student/myapplicationxxx/MainActivity.java
+++ app/src/main/java/com/example/student/myapplicationxxx/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
@@ -20,15 +19,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Intent in = new Intent();
+        in.setClass(MainActivity.this, Cover.class);
+        startActivity(in);
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cover);
-        Runnable rab = new Runnable() {
-            @Override
-            public void run() {
-                setContentView(R.layout.activity_main);//設定 Layout
-            }
-        };
-        new Handler().postDelayed(rab, 5000);    // 5 sec 後轉跳主頁
+        setContentView(R.layout.activity_main);
+
 //                runLoging();                               //執行登入程序
 
         // ==========檢查Network==========
